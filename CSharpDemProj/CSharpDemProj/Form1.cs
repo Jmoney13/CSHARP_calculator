1358d1d1230fb74a0b16e4f72b003c89c81d1662
Jon Messmer <38733091+Jmoney13@users.noreply.github.com>
2022-05-12 00:38:27 -0400

    Add files via upload

---
+++ CSharpDemProj/CSharpDemProj/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace CSharpDemProj
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "Add Me!";
+            textBox2.Text = "Add Me too!!";
+            textBox3.Text = "I'm the Sum!";
+        }
+
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            int firstValue = Int32.Parse(textBox1.Text);
+            int secondValue = Int32.Parse(textBox2.Text);
+            
+            int sum = firstValue + secondValue;
+            textBox3.Text = sum.ToString("N1");
+        }
+
+       
+    }
+}
